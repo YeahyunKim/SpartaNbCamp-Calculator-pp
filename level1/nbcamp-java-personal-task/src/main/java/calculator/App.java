209de09e3d2f5db83b1487dda577c496ad36ceae
Yeahyun Kim <insidesy4@gmail.com>
2024-04-25 14:26:04 +0900

    [Feat] 사칙연산 기호를 받을 char operator 변수 생성 #2

--- level1/nbcamp-java-personal-task/src/main/java/calculator/App.java
+++ level1/nbcamp-java-personal-task/src/main/java/calculator/App.java
@@ -11,5 +11,8 @@ public class App {
 
         System.out.print("두 번째 숫자를 입력하세요: ");
         int num2 = input.nextInt(); //두 번째 숫자 값을 입력하고 num1에 저장
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = input.next().charAt(0); //사칙연산 기호 값을 입력하고 operator에 저장
     }
 }
