9e7344a94c08d11258f4ea8080572a4cdea2a9be
Yeahyun Kim <insidesy4@gmail.com>
2024-04-26 10:53:15 +0900

    [Add] level 2 파일 추가 #10

---
+++ level2/nbcamp-java-personal-task/src/main/java/calculator/App.java
@@ -0,0 +1,11 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Objects;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
