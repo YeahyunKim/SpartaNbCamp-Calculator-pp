af50a251e4e1bf47bbad0802957e4d3718fa1104
Yeahyun Kim <insidesy4@gmail.com>
2024-04-29 11:49:22 +0900

    [Fix] level 2-8 사칙연산 / 원넓이 setter remove 메서드 Calculator Class에서 이동 #19

--- level2/nbcamp-java-personal-task/src/main/java/calculator/ArithmeticCalculator.java
+++ level2/nbcamp-java-personal-task/src/main/java/calculator/ArithmeticCalculator.java
@@ -1,4 +1,14 @@
 package calculator;
 
-public class ArithmeticCalculator {
+public class ArithmeticCalculator extends Calculator{
+    // Setter 메서드
+    public void setResultList(double result) {
+        this.getResultList().add(result);
+    }
+
+    // Remove 사칙연산 첫 번째 결과값 삭제
+    public void removeResult() {
+        this.getResultList().remove(0);
+    }
+
 }
--- level2/nbcamp-java-personal-task/src/main/java/calculator/CircleCalculator.java
+++ level2/nbcamp-java-personal-task/src/main/java/calculator/CircleCalculator.java
@@ -1,4 +1,17 @@
 package calculator;
 
-public class CircleCalculator {
+import java.util.ArrayList;
+
+public class CircleCalculator extends Calculator {
+
+
+    // Setter 메서드
+    public void setCircleAreaResultList(double result) {
+        this.getCircleAreaResultList().add(result);  //
+    }
+
+    // Remove 원넓이 첫 번째 결과값 삭제
+    public void removeCircleAreaResult() {
+        this.getCircleAreaResultList().remove(0);
+    }
 }
